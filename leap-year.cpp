0170a72f7b9a5dedd7c13a8563a7abd1d6e0ce12
yoana stoyanova <82516108+yoana-stoyanova@users.noreply.github.com>
2021-05-02 17:08:29 +0100

    Add files via upload

---
+++ leap-year.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int year;
+    cout<<"year = "; cin>>year;
+    if(year%4==0)
+    {
+        if(year%100==0)
+        {
+          if(year%400==0) cout<<"It's a leap year";
+          else cout<<"It's not a leap year.";
+        }
+        else cout<<"It's a leap year.";
+    }
+    else cout<<"It's not a leap year.";
+
+
+    return 0;
+}
